6adc7c4408df9c231bfc2a5d452a868d65e9ad6d
Ragith Saba <ragith.coolkid@gmail.com>
2021-01-22 15:10:01 -0500

    feat: add request images

--- js/hero-slider.js
+++ js/hero-slider.js
@@ -1,4 +1,4 @@
-var images = ["/bg_7.jpg", "/bg_8.jpg", "/bg_9.jpg", "/bg_10.jpg"];
+var images = ["/bg_7.jpg", "/bg_11.jpg","/bg_12.jpg", "/bg_13.jpg", "/bg_14.jpg", "/bg_9.jpg", "/bg_10.jpg"];
 var i = 0;
 var x = $("#hero-slide");
 $("#hero-slide").css("background-image", "url(images/" + images[i] + ")");
